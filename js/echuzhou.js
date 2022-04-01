db708acff3d4974791c5e25b5cd44baa693edf13
douya888 <2087148022@qq.com>
2022-04-01 18:33:11 +0800

    main

--- js/echuzhou.js
+++ js/echuzhou.js
@@ -1,6 +1,16 @@
 var body = $response.body;
 var url = $request.url;
+const p1 = '/init/start_ad';
+const p2 = 'area_code=&channel_id=0&city=&cursor=0&page=1&tab_id=1';
 
+
+if (url.indexOf(p1) != -1) {
+    obj.data="";//清除开屏广告
+    body = JSON.stringify(obj);
+    console.log('跳过广告');
+    $done(body);
+}
+if (url.indexOf(p2) != -1) {
 
     //var body = $response.body;
     var obj = JSON.parse(body);
@@ -13,6 +23,7 @@ var url = $request.url;
     console.log('APP-E滁州-净化')
 	//console.log(body)
     $done(body);
+}
  
 
 
